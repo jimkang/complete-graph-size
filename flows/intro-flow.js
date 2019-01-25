f62ef4b9a3026f4551a182c9918baac40cee1f7f
Jim Kang <jimkang@gmail.com>
2019-01-25 09:38:28 -0500

    Refactor.

--- flows/intro-flow.js
+++ flows/intro-flow.js
@@ -2,13 +2,22 @@ var renderEdges = require('../dom/render-edges');
 var renderPoints = require('../dom/render-points');
 var getCompleteGraphVertexPositions = require('../get-complete-graph-vertex-positions');
 var getCompleteGraph = require('../get-complete-graph');
-var colorScales = require('d3-scale-chromatic');
-var curry = require('lodash.curry');
 var getPyramid = require('../get-pyramid');
 
 function introFlow() {
+  makeGraphInPanel({
+    rootSelector: '#small-intro-graph-board',
+    numberOfVertices: 21
+  });
+  makeTriangleInPanel({
+    rootSelector: '#small-intro-triangular-number-board',
+    pointsPerSide: 20
+  });
+}
+
+function makeGraphInPanel({ rootSelector, numberOfVertices }) {
   var vertices = getCompleteGraphVertexPositions({
-    numberOfVertices: 25,
+    numberOfVertices,
     width: 100,
     height: 100,
     margin: 10
@@ -18,19 +27,20 @@ function introFlow() {
   renderEdges({
     edges,
     className: 'graph-edge',
-    rootSelector: '#intro-graph-example-edge-root',
-    colorAccessor: curry(getEdgeColor)(edges.length) // 'rgb(237, 121, 83)'
+    rootSelector: rootSelector + ' .graph-edge-root',
+    colorAccessor: 'rgb(151, 237, 123)'
   });
 
   renderPoints({
     points: vertices,
     className: 'graph-vertex',
-    rootSelector: '#intro-graph-example-vertex-root',
-    labelAccessor: getIndex
+    rootSelector: rootSelector + ' .graph-vertex-root'
   });
+}
 
+function makeTriangleInPanel({ rootSelector, pointsPerSide }) {
   var pyramid = getPyramid({
-    pointsPerSide: 20,
+    pointsPerSide,
     width: 100,
     height: 100,
     margin: 0
@@ -39,16 +49,8 @@ function introFlow() {
   renderPoints({
     points: pyramid.points,
     className: 'graph-vertex',
-    rootSelector: '#example-triangle-dot-root'
+    rootSelector: rootSelector + ' .triangle-dot-root'
   });
-}
-
-function getIndex(d, i) {
-  return i;
-}
-
-function getEdgeColor(edgeCount, d, i) {
-  return colorScales.interpolateViridis(i / edgeCount);
 }
 
 module.exports = introFlow;
