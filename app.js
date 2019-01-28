9553db9de0d91f6b609a7641b9f40efb8186cedb
Jim Kang <jimkang@gmail.com>
2019-01-28 09:41:00 -0500

    Put explanation of relationship between vertices and edgex in a separate step. Refactor: Wrap a constructor around graphExampleFlow so that it can be used in two different places.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var handleError = require('handle-error-web');
-var graphExampleFlow = require('./flows/graph-example-flow');
+var GraphExampleFlow = require('./flows/graph-example-flow');
 var triangularNumberFlow = require('./flows/triangular-number-flow');
 var introFlow = require('./flows/intro-flow');
+
+var graphExampleFlow = GraphExampleFlow({
+  stepSelector: '#complete-graph-step'
+});
+var veRelationshipGraphFlow = GraphExampleFlow({
+  stepSelector: '#v-e-relationship-step'
+});
 
 (function go() {
   window.onerror = reportTopLevelError;
   introFlow();
   graphExampleFlow();
   triangularNumberFlow();
+  veRelationshipGraphFlow();
 })();
 
 function reportTopLevelError(msg, url, lineNo, columnNo, error) {
--- flows/graph-example-flow.js
+++ flows/graph-example-flow.js
@@ -1,10 +1,3 @@
-var controlInitialized = false;
-var exampleGraphVerticesControl = document.getElementById(
-  'example-graph-vertices-control'
-);
-var vertexCountLabel = document.querySelector(
-  '#complete-graph-step .vertex-count-label'
-);
 var renderEdges = require('../dom/render-edges');
 var renderPoints = require('../dom/render-points');
 var getCompleteGraphVertexPositions = require('../get-complete-graph-vertex-positions');
@@ -12,35 +5,46 @@ var getCompleteGraph = require('../get-complete-graph');
 var colorScales = require('d3-scale-chromatic');
 var curry = require('lodash.curry');
 
-function graphExampleFlow() {
-  if (!controlInitialized) {
-    exampleGraphVerticesControl.addEventListener('change', graphExampleFlow);
-    controlInitialized = true;
+function GraphExampleFlow({ stepSelector }) {
+  var controlInitialized = false;
+  var exampleGraphVerticesControl = document.querySelector(
+    stepSelector + ' .graph-vertices-control'
+  );
+  var vertexCountLabel = document.querySelector(
+    stepSelector + ' .vertex-count-label'
+  );
+  return graphExampleFlow;
+
+  function graphExampleFlow() {
+    if (!controlInitialized) {
+      exampleGraphVerticesControl.addEventListener('change', graphExampleFlow);
+      controlInitialized = true;
+    }
+
+    var numberOfVertices = Math.round(+exampleGraphVerticesControl.value);
+    vertexCountLabel.textContent = numberOfVertices;
+    var vertices = getCompleteGraphVertexPositions({
+      numberOfVertices,
+      width: 100,
+      height: 100,
+      margin: 5
+    });
+    var edges = getCompleteGraph({ vertices });
+
+    renderEdges({
+      edges,
+      className: 'graph-edge',
+      rootSelector: stepSelector + ' .graph-edge-root',
+      colorAccessor: curry(getEdgeColor)(edges.length) // 'rgb(237, 121, 83)'
+    });
+
+    renderPoints({
+      points: vertices,
+      className: 'graph-vertex',
+      rootSelector: stepSelector + ' .graph-vertex-root',
+      labelAccessor: getIndex
+    });
   }
-
-  var numberOfVertices = Math.round(+exampleGraphVerticesControl.value);
-  vertexCountLabel.textContent = numberOfVertices;
-  var vertices = getCompleteGraphVertexPositions({
-    numberOfVertices,
-    width: 100,
-    height: 100,
-    margin: 5
-  });
-  var edges = getCompleteGraph({ vertices });
-
-  renderEdges({
-    edges,
-    className: 'graph-edge',
-    rootSelector: '#complete-graph-example-edge-root',
-    colorAccessor: curry(getEdgeColor)(edges.length) // 'rgb(237, 121, 83)'
-  });
-
-  renderPoints({
-    points: vertices,
-    className: 'graph-vertex',
-    rootSelector: '#complete-graph-example-vertex-root',
-    labelAccessor: getIndex
-  });
 }
 
 function getIndex(d, i) {
@@ -51,4 +55,4 @@ function getEdgeColor(edgeCount, d, i) {
   return colorScales.interpolateViridis(i / edgeCount);
 }
 
-module.exports = graphExampleFlow;
+module.exports = GraphExampleFlow;
